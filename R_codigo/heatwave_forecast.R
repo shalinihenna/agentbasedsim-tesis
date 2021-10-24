cc2303006123d9e05a62fc98ccb410dceff413c6
shalinihenna <shalini.ramchandani@usach.cl>
2021-10-24 01:25:38 -0300

    Creo que se corrigió problema de stl, pero seguir verificando + plot de Tmax datos

--- R_codigo/heatwave_forecast.R
+++ R_codigo/heatwave_forecast.R
@@ -16,11 +16,15 @@ library(dplyr)
 temp <- read_excel("C:/Users/shali/OneDrive/Escritorio/Tesis/agentbasedsim-tesis/Datos/threat_OlasCalor/Tmax-daily2.xlsx", col_types = "numeric")
 temp <- temp %>% gather(key = "year", value="temperature", -date) 
 temp_ts <- ts(data = temp[,3], frequency = 366, start = c(1980,1))
-temp_ts <- window(temp_ts, start = c(1980,1))
+temp_ts <- window(temp_ts, start = c(1980,1), end=c(1985,1))
 
 #Plot time series 
 autoplot(temp_ts) + ylab("Temperature °C") + xlab("Year") + ggtitle("Daily Max Temperature RM 1980-2020") #+ scale_x_date(date_labels = '%b\n%Y', breaks = '1 year', minor_breaks = '1 month') 
 
 # Decomposition of the time series in 3 components: Seasonality, Trend and Remainder (Estacionalidad, Tendencia y Resto)
-temp_decomp <- stl(temp_ts[,1], s.window = 'periodic', na.action = na.pass)
-autoplot(temp_decomp) + ggtitle("Decomposition (Seasonal, Trend and Remainder)")
+temp_decomp <- stl(temp_ts[,1], s.window = 'periodic', na.action = na.remove)
+# autoplot(temp_decomp) + ggtitle("Decomposition (Seasonal, Trend and Remainder)")
+#print(temp_ts, calendar = TRUE)
+#temp_ts3 <- na.remove(temp_ts) 
+
+autoplot(temp_decomp)+ theme_bw()
