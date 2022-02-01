efe5e994a0d18465fff5e9c45167f9ed99289028
shalinihenna <shalini.ramchandani@usach.cl>
2022-02-01 01:27:47 -0300

    plot code in R. Preprocessing of the csv WIP

---
+++ R_codigo/plots.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+#Lectura del csv
+mercadoMayoristaTotal <- read.csv('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/Mercado_Mayorista_total.csv', sep=";")
+
+#Sección para procesar el archivo y convertirlo en dataframe
+#hola <- strsplit(mercadoMayoristaTotal[[1]], split="\:\:")
+#mercadoMayoristaTotal[1] %>% unnest_wider(mercadoMayoristaTotal[1])
+#map\\(\\[|\\]\\)|\\::|\\,
+
+
+
+#División de unidades y kilos para generar dos graficos
+unidades <- c("Ajo", "Alcachofa","Apio","BrÃ³coli","Brócoli","Choclo","Coliflor","Lechuga","MelÃ³n","Melón","Pepino ensalada","Repollo","Sandia","Zapallo italiano")
+kilos <- c("Arveja Verde","Cebolla","Espinaca","Haba","Poroto granado","Poroto verde","Tomate","Zanahoria","Frutilla")
+
+products_unidades <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% unidades)
+products_kilos <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% kilos)
+
+#Plot del geombar (grafico de barras)
+ggplot(products_unidades, aes(x=Producto, y=Volumen)) + geom_bar(stat = "identity")
+ggplot(products_kilos, aes(x=Producto, y=Volumen)) + geom_bar(stat = "identity")
