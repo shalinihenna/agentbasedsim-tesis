11f12931249b3fb62c99e6171191191b55c5239a
shalinihenna <shalini.ramchandani@usach.cl>
2022-02-03 01:22:39 -0300

    Production plots done for each experiment. Prices plots WIP

--- R_codigo/plots.R
+++ R_codigo/plots.R
@@ -2,16 +2,55 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-#Lectura del csv
-mercadoMayoristaTotal <- read.csv('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/Mercado_Mayorista_total_prueba.csv', sep=";")
-
 #División de unidades y kilos para generar dos graficos
 unidades <- c("Ajo", "Alcachofa","Apio","BrÃ³coli","Brócoli","Choclo","Coliflor","Lechuga","MelÃ³n","Melón","Pepino ensalada","Repollo","Sandia","Zapallo italiano")
 kilos <- c("Arveja Verde","Cebolla","Espinaca","Haba","Poroto granado","Poroto verde","Tomate","Zanahoria","Frutilla")
 
-products_unidades <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% unidades)
-products_kilos <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% kilos)
+for (expe in c(1,2,3,4)){
+  #PARTE 1: Plots mercado mayorista
+  #Lectura del csv
+  mercadoMayoristaTotal <- read.csv(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/Mercado_Mayorista_total_Mayo2021.csv'), sep=";")
+  
+  mm_unidades <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% unidades)
+  mm_kilos <- subset(mercadoMayoristaTotal, mercadoMayoristaTotal$Producto %in% kilos)
+  
+  #Plot del geombar (grafico de barras)
+  ggplot(mm_unidades, aes(x=Producto, y=mercadoMayoristaVolumenTotal)) + geom_bar(stat = "identity", fill = "yellow") + ylab("Volumen [un.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotMayoristaUnidades.png'), width = 9, height = 7)
+  ggplot(mm_kilos, aes(x=Producto, y=mercadoMayoristaVolumenTotal)) + geom_bar(stat = "identity", fill = "yellow") + ylab("Volumen [kg.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotMayoristaKilos.png'), width = 9, height = 7)
+  
+  #-----------------------------------------------
+  
+  #PARTE 2: Plots feriante
+  #Lectura del csv
+  ferianteTotal <- read.csv(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/Feriantes_total_Mayo2021.csv'), sep=";")
+  
+  fer_unidades <- subset(ferianteTotal, ferianteTotal$Producto %in% unidades)
+  fer_kilos <- subset(ferianteTotal, ferianteTotal$Producto %in% kilos)
+  
+  #Plot del geombar (grafico de barras)
+  ggplot(fer_unidades, aes(x=Producto, y=feriantesVolumenTotal)) + geom_bar(stat = "identity", fill = "green") + ylab("Volumen [un.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotFerianteUnidades.png'), width = 9, height = 7)
+  ggplot(fer_kilos, aes(x=Producto, y=feriantesVolumenTotal)) + geom_bar(stat = "identity", fill = "green") + ylab("Volumen [kg.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotFerianteKilos.png'), width = 9, height = 7)
+  
+  #-----------------------------------------------
+  
+  #PARTE 3: Plots consumidor
+  consumerTotal <- read.csv(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/Consumidores_total_Mayo2021.csv'), sep=";")
+  
+  con_unidades <- subset(consumerTotal, consumerTotal$Producto %in% unidades)
+  con_kilos <- subset(consumerTotal, consumerTotal$Producto %in% kilos)
+  
+  #Plot del geombar (grafico de barras)
+  ggplot(con_unidades, aes(x=Producto, y=consumersVolumenTotal)) + geom_bar(stat = "identity", fill = "blue") + ylab("Volumen [un.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotConsumerUnidades.png'), width = 9, height = 7)
+  ggplot(con_kilos, aes(x=Producto, y=consumersVolumenTotal)) + geom_bar(stat = "identity", fill = "blue") + ylab("Volumen [kg.]")
+  ggsave(paste0('C:/Users/shali/OneDrive/Escritorio/GAMA_1.8.1/tesis/gama_codigo/models/resultados/finales/exp',expe,'-finales/plotConsumerKilos.png'), width = 9, height = 7)
+  
+}
 
-#Plot del geombar (grafico de barras)
-ggplot(products_unidades, aes(x=Producto, y=Volumen)) + geom_bar(stat = "identity")
-ggplot(products_kilos, aes(x=Producto, y=Volumen)) + geom_bar(stat = "identity")
+
+
+
